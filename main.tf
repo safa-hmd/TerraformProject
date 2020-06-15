1b13345974537b317a3945b2f84b9ffd571422b8
safa-hmd <64669988+safa-hmd@users.noreply.github.com>
2020-06-15 19:35:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
-provider "google" {
-  version = "3.5.0"
-
-  credentials = file("<NAME>.json")
-
-  project = "<PROJECT_ID>"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
 }
+
